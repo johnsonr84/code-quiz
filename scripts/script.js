b2887c082c1af592f66148ad37d72e625d36e6bf
Robert Johnson <robertcjohnson1984@gmail.com>
2021-01-09 10:17:07 -0700

    add event listener on start button

--- scripts/script.js
+++ scripts/script.js
@@ -8,38 +8,8 @@ document.createElement('button2') = answer2
 document.createElement('button3') = answer3
 document.createElement('button4') = answer4
 
-// CODE QUESTIONS ==============================================
-const questions = [
-	{
-		question: 'Commonly used data types DO NOT Include ____________ .',
-		options: [ 'A) strings', 'B) booleans', 'C) alerts', 'D) numbers' ],
-		answer: 2
-	},
-	{
-		question: 'The condition in an if / else statement is enclosed within __________ .',
-		options: [ 'A) quotes', 'B) curly brackets', 'C) parenthesis', 'D) square brackets' ],
-		answer: 2
-	},
-	{
-		question: 'Arrays in JavaScript can store __________ .',
-		options: [ 'A) numbers and strings', 'B) other Arrays', 'C) booleans', 'D) all of the above' ],
-		answer: 3
-    },
-    {
-		question: 'String values must be enclosed within __________ when being assigned to variables.',
-		options: [ 'A) commas', 'B) curly brackets', 'C) quotes', 'D) parenthesis' ],
-		answer: 1
-    },
-    {
-		question: 'A very useful tool used during development and debugging for printing content to the debugger is __________ .',
-		options: [ 'A) JavaScript', 'B) terminal / bash', 'C) for loops', 'D) console.log' ],
-		answer: 3
-    },
-];
-
-
-// add event listener on start 
-
+// START BUTTON EVENT LISTENER	
+document.getElementById("startBtn").addEventListener("click")
 
 // QUESTIONS AND ANSWERS CHANGE
 // document.createElement('submit') = submit
