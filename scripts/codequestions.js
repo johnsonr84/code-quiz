f2b7d6facb64ebac4847ca23b9a3d3c23cf66243
Robert Johnson <robertcjohnson1984@gmail.com>
2021-01-09 10:08:04 -0700

    add code questions to separate file

---
+++ scripts/codequestions.js
@@ -0,0 +1,28 @@
+// CODE QUESTIONS ==============================================
+const questions = [
+	{
+		question: 'Commonly used data types DO NOT Include ____________ .',
+		options: [ 'A) strings', 'B) booleans', 'C) alerts', 'D) numbers' ],
+		answer: 2
+	},
+	{
+		question: 'The condition in an if / else statement is enclosed within __________ .',
+		options: [ 'A) quotes', 'B) curly brackets', 'C) parenthesis', 'D) square brackets' ],
+		answer: 2
+	},
+	{
+		question: 'Arrays in JavaScript can store __________ .',
+		options: [ 'A) numbers and strings', 'B) other Arrays', 'C) booleans', 'D) all of the above' ],
+		answer: 3
+    },
+    {
+		question: 'String values must be enclosed within __________ when being assigned to variables.',
+		options: [ 'A) commas', 'B) curly brackets', 'C) quotes', 'D) parenthesis' ],
+		answer: 1
+    },
+    {
+		question: 'A very useful tool used during development and debugging for printing content to the debugger is __________ .',
+		options: [ 'A) JavaScript', 'B) terminal / bash', 'C) for loops', 'D) console.log' ],
+		answer: 3
+    },
+];
