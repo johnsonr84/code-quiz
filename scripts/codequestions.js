46ca944c13c948b4cf3a4a5d81e89f75a2cde22a
Robert Johnson <robertcjohnson1984@gmail.com>
2021-01-21 20:30:01 -0700

    add couple more questions to codequestions.js

--- scripts/codequestions.js
+++ scripts/codequestions.js
@@ -2,27 +2,37 @@
 const questions = [
 	{
 		question: 'Commonly used data types DO NOT Include ____________ .',
-		options: [ 'A) strings', 'B) booleans', 'C) alerts', 'D) numbers' ],
+		options: ['A) strings', 'B) booleans', 'C) alerts', 'D) numbers'],
 		answer: 2
 	},
 	{
 		question: 'The condition in an if / else statement is enclosed within __________ .',
-		options: [ 'A) quotes', 'B) curly brackets', 'C) parenthesis', 'D) square brackets' ],
+		options: ['A) quotes', 'B) curly brackets', 'C) parenthesis', 'D) square brackets'],
 		answer: 2
 	},
 	{
 		question: 'Arrays in JavaScript can store __________ .',
-		options: [ 'A) numbers and strings', 'B) other Arrays', 'C) booleans', 'D) all of the above' ],
+		options: ['A) numbers and strings', 'B) other Arrays', 'C) booleans', 'D) all of the above'],
 		answer: 3
-    },
-    {
+	},
+	{
 		question: 'String values must be enclosed within __________ when being assigned to variables.',
-		options: [ 'A) commas', 'B) curly brackets', 'C) quotes', 'D) parenthesis' ],
+		options: ['A) commas', 'B) curly brackets', 'C) quotes', 'D) parenthesis'],
 		answer: 1
-    },
-    {
+	},
+	{
 		question: 'A very useful tool used during development and debugging for printing content to the debugger is __________ .',
-		options: [ 'A) JavaScript', 'B) terminal / bash', 'C) for loops', 'D) console.log' ],
+		options: ['A) JavaScript', 'B) terminal / bash', 'C) for loops', 'D) console.log'],
 		answer: 3
-    },
+	},
+	{
+		question: 'How does a FOR loop start?.',
+		options: ['A) for i = 1 to 5', 'B) for (i <= 5; 1++', 'C) for (let i = 0; i <= 5)', 'D) for (let i = 0, i <= 5; i++)'],
+		answer: 3
+	},
+	{
+		question: 'What will the following code return: Boolean(5 > 10)',
+		options: ['false', 'true', 'NaN', '10 > 9'],
+		answer: 0
+}
 ];
